4152f498bf09fbd14dc4151cd340701c26ae2ea0
zzp0425 <412960861@qq.com>
2016-04-07 16:43:54 +0800

    java_web start success & start learning Bootstrap

---
+++ src/main/java/com/web/dao/user/UserDAO.java
@@ -0,0 +1,8 @@
+package com.web.dao.user;
+
+/**
+ * Created by 周振平
+ * on 2016/4/7.
+ */
+public class UserDAO {
+}
---
+++ src/main/java/com/web/model/user/User.java
@@ -0,0 +1,8 @@
+package com.web.model.user;
+
+/**
+ * Created by 周振平
+ * on 2016/4/7.
+ */
+public class User {
+}
